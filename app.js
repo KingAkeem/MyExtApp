0950d61ab30b78aac5246c0a7f8a634cd526edc8
KingAkeem <akeemtlking@gmail.com>
2018-05-27 15:12:16 -0400

    This is a test project

---
+++ app.js
@@ -0,0 +1,67 @@
+// Creation of data model
+Ext.define('PlayerModel', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'name', mapping: 'name'},
+        {name: 'pos', mapping: 'pos'},
+        {name: 'ppg', mapping: 'ppg'},
+        {name: 'age', mapping: 'age'}
+    ]
+});
+
+var playerStore = Ext.create('Ext.data.Store', {
+    model: 'PlayerModel',
+    data: [
+        {name: "LeBron James", pos: 'SF', ppg: 27.5, age: "33"},
+        {name: "Michael Jordan", pos: 'SG', ppg: 30.1, age: "55"},
+        {name: "Kobe Bryant", pos: 'SG', ppg: 25.0, age: "39"}
+    ]
+});
+
+
+Ext.onReady(function() {
+    // Creation of first grid
+    Ext.create('Ext.grid.Panel', {
+        store: playerStore,
+        stripeRows: true,
+        title: 'Best NBA Players',  // Title for the grid
+        renderTo:'gridDiv',         // Div id where the grid has to be rendered
+        layout: 'auto',
+        collapsible: true,             // property to collapse grid
+        enableColumnMove:true,              // property which allows column to move to different position by dragging that column.
+        enableColumnResize:true,        // property which allows to resize column run time.
+        listeners: {
+            click: function () {
+               var current_cell = this.getCell(true);
+               current_cell.focus();
+               console.log(current_cell);
+            }
+        },
+        columns:
+            [{
+                header: 'Name',
+                dataIndex: 'name',
+                id : 'name',
+                flex:  1,        // property defines the amount of space this column is going to take in the grid container with respect to all.
+                sortable: true,  // property to sort grid column data.
+                hideable: false   // property which allows column to be hidden run time on user request.
+            },{
+                header: 'Position',
+                dataIndex: 'pos',
+                flex: .5,
+                sortable: true,
+                hideable: true   // this column will not be available to be hidden.
+            }, {
+                header: 'PPG',
+                dataIndex: 'ppg',
+                flex: .5,
+                sortable: true,
+            }, {
+                header: 'Age',
+                dataIndex: 'age',
+                flex: .5,
+                sortable: true,
+                hideable: true
+            }]
+    });
+});
